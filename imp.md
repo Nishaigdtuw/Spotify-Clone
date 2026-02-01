a425210ebb176ad0907ae8e665e3ac03b32b5f05
Nisha Kumari <nk568972@gmail.com>
2026-02-01 21:51:49 +0530

    Initial commit: Spotify Clone

---
+++ imp.md
@@ -0,0 +1,36 @@
+flex-direction:row
+ye default hota h
+
+isme agr align-items:centre krenga toh ye columns wise means upar niche centre me layega
+and isme agr justify-content → left/right control karega
+
+flex-direction:column me iska opposite hota h
+
+//font size
+rem root element (html) ke font-size par depend karta hai.
+By default:
+
+html {
+  font-size: 16px;
+}
+
+
+Toh:
+
+1rem = 16px
+1.3rem ≈ 20.8px
+font-size: 1.3rem; ka matlab
+font-size: 1.3rem;
+
+
+➡️ Text root font-size ka 1.3 guna ho jaayega
+➡️ Screen chhoti–badi ho, ya user zoom kare → size smartly adjust hota hai
+
+//padding
+padding: vertical horizontal;
+padding: 0 12px;
+👉 Breakdown
+Top & Bottom padding = 0
+
+Left & Right padding = 12px
+
---
+++ script.js
@@ -0,0 +1,141 @@
+console.log("welcome");
+
+//intitialse the variable
+let songIndex=1;
+let audioElement = new Audio("songs/1.mp3");
+
+//masterPlay → play/pause button
+//myProgressBar → range slider (seek bar)
+//gif → playing animation
+//songItems → saare song rows (NodeList ko Array me convert kiya)
+//Array.from() isliye use hua kyunki forEach lagana hai
+
+let masterPlay=document.getElementById("masterPlay");
+let myProgressBar=document.getElementById("myProgressBar");
+let gif=document.getElementById("gif");
+let songItems=Array.from(document.getElementsByClassName("songItem"));
+let masterSongName=document.getElementById("masterSongName");
+
+
+
+let songs = [
+    { songName: "Let me love you", filePath: "songs/1.mp3", coverPath: "covers/1.jpg" },
+    { songName: "hawayein", filePath: "songs/2.mp3", coverPath: "covers/2.jpg" },
+    { songName: "Bandeya Rey Bandeya", filePath: "songs/3.mp3", coverPath: "covers/3.jpg" },
+    { songName: "Gehra Hua / Dhurandhar", filePath: "songs/4.mp3", coverPath: "covers/4.jpg" },
+    { songName: "Agar Tum Sath Ho ", filePath: "songs/5.mp3", coverPath: "covers/5.jpg" },
+    { songName: "Satranga ", filePath: "songs/6.mp3", coverPath: "covers/6.jpg" },
+    { songName: "Teri Deewani", filePath: "songs/7.mp3", coverPath: "covers/7.jpg" },
+    { songName: "Bhula Dena", filePath: "songs/8.mp3", coverPath: "covers/8.jpg" },
+    { songName: "Tauba Tauba", filePath: "songs/9.mp3", coverPath: "covers/9.jpg" },
+    { songName: "Chaleya", filePath: "songs/10.mp3", coverPath: "covers/10.jpg" }
+  ];
+
+ // forEach array ke har item pe ek-ek baar loop chalata hai
+ //arr.forEach((item, index)     //syntax
+songItems.forEach((element,i)=>{
+    console.log(element,i);
+    element.getElementsByTagName("img")[0].src=songs[i].coverPath;
+    element.getElementsByClassName("songName")[0].innerText=songs[i].songName;
+
+})
+
+// audioElement.play();
+
+//handle Play/pause click
+masterPlay.addEventListener("click",()=>{
+    if (audioElement.paused || audioElement.currentTime<=0){
+        audioElement.play();
+        masterPlay.classList.remove("fa-circle-play");
+        masterPlay.classList.add("fa-circle-pause");
+        gif.style.opacity=1;
+
+    }
+    else{
+        audioElement.pause();
+        masterPlay.classList.remove("fa-circle-pause");
+        masterPlay.classList.add("fa-circle-play");
+        gif.style.opacity=0;
+
+    }
+})
+
+//update seekbar
+audioElement.addEventListener("timeupdate",()=>{
+    //currentTime → kitna song chal chuka
+    //duration → total song length
+    letprogress=parseInt((audioElement.currentTime/audioElement.duration)*100); //in percent 
+    myProgressBar.value=progress;
+    //Song chalne ke sath slider bhi move karta hai
+
+})
+
+//Seek bar se song control karna
+myProgressBar.addEventListener("change",()=>{
+    audioElement.currentTime=(myProgressBar.value * audioElement.duration)/100;
+})
+
+//Har song ke play icon ko:
+//pause htao and play lgao
+const makeAllPlays=()=>{
+    Array.from(document.getElementsByClassName("songItemPlay")).forEach((element)=>{
+        element.classList.remove("fa-circle-pause");
+        element.classList.add("fa-circle-play");
+    })
+}
+
+Array.from(document.getElementsByClassName("songItemPlay")).forEach((element)=>
+    element.addEventListener("click",(e)=>{
+        makeAllPlays();
+        songIndex=parseInt(e.target.id);
+        //e.target kya hai?
+        // jis song ke play icon pe click hua, wahi element
+       
+        e.target.classList.remove("fa-circle-play");
+        e.target.classList.add("fa-circle-pause");
+        audioElement.src=`songs/${songIndex}.mp3`;
+        masterSongName.innerHTML=songs[songIndex-1].songName;
+        audioElement.currentTime=0;
+        audioElement.play();
+        gif.style.opacity=1;
+        // masterPlay bottom wala main play/pause button hai
+        masterPlay.classList.remove("fa-circle-play");
+        masterPlay.classList.add("fa-circle-pause");
+    })
+)
+
+document.getElementById("next").addEventListener("click",()=>{
+    if (songIndex>=10){
+        songIndex=1;
+
+    }
+    else{
+        songIndex+=1;
+
+    }
+    audioElement.src=`songs/${songIndex}.mp3`;
+    masterSongName.innerHTML=songs[songIndex-1].songName;
+    audioElement.currentTime=0;
+    audioElement.play();
+    gif.style.opacity=1;
+    masterPlay.classList.remove("fa-circle-play");
+    masterPlay.classList.add("fa-circle-pause");
+})
+
+document.getElementById("previous").addEventListener("click",()=>{
+    if (songIndex<=1){
+        songIndex=1;
+
+    }
+    else{
+        songIndex-=1;
+
+    }
+    audioElement.src=`songs/${songIndex}.mp3`;
+    masterSongName.innerHTML=songs[songIndex-1].songName;
+    audioElement.currentTime=0;
+    audioElement.play();
+    gif.style.opacity=1;
+    masterPlay.classList.remove("fa-circle-play");
+    masterPlay.classList.add("fa-circle-pause");
+})
